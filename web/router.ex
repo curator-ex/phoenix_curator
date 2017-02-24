656912d5843addf30250a04efe753e4e63aea182
Eric Sullivan <eric.sullivan@annkissam.com>
2017-02-24 16:01:03 -0500

    add a 'secret' page

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule PhoenixCurator.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    get "/secret", PageController, :secret
   end
 
   # Other scopes may use custom stacks.
